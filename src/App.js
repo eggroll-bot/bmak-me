e684c6031bcf42b07a7f60b53d4222042063fde0
Brian Mak <eggrollbot@gmail.com>
2021-06-16 13:50:21 -0700

    Added intro text to landing page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import "./App.css";
+import { Typography } from "@material-ui/core";
 
 function App() {
+	const theme = createMuiTheme( {
+		palette: {
+			type: "dark",
+		}
+	} );
+
 	return (
 		<div className="App">
-			<h1>Test</h1>
+			<MuiThemeProvider theme={theme}>
+				<CssBaseline />
+				<Typography variant="h2" gutterBottom={true}>Welcome to my portfolio.</Typography>
+				<Typography variant="h3">I&apos;m Brian.</Typography>
+			</MuiThemeProvider>
 		</div>
 	);
 }
