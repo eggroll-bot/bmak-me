af048a72535eac84f0931f1d9926827002e7c9ae
Brian Mak <eggrollbot@gmail.com>
2021-06-16 16:36:48 -0700

    Changed up text and added buttons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
-import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import "./App.css";
-import { Typography } from "@material-ui/core";
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
+import { Box, Button, CssBaseline, Grid, Typography } from "@material-ui/core";
+import { Code as CodeIcon, /*GitHub as GitHubIcon,*/ Info as InfoIcon, /*LinkedIn as LinkedInIcon*/ } from "@material-ui/icons";
 
 function App() {
 	const theme = createMuiTheme( {
 		palette: {
 			type: "dark",
+			primary: {
+				main: "#27ae60"
+			}
 		}
 	} );
 
@@ -15,8 +18,22 @@ function App() {
 		<div className="App">
 			<MuiThemeProvider theme={theme}>
 				<CssBaseline />
-				<Typography variant="h2" gutterBottom={true}>Welcome to my portfolio.</Typography>
-				<Typography variant="h3">I&apos;m Brian.</Typography>
+
+				<Box mb={3}>
+					<Typography align="center" variant="h2" gutterBottom={true}>👋 Hey, I&apos;m Brian.</Typography>
+				</Box>
+
+				<Box mb={3}>
+					<Grid container align="center" justify="center" spacing={5}>
+						<Grid item>
+							<Button color="primary" size="medium" startIcon={<InfoIcon />} variant="outlined">About Me</Button>
+						</Grid>
+
+						<Grid item>
+							<Button color="primary" size="medium" startIcon={<CodeIcon />} variant="outlined">Portfolio</Button>
+						</Grid>
+					</Grid>
+				</Box>
 			</MuiThemeProvider>
 		</div>
 	);
