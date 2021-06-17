7c528af2ceaff03f369427299e9ac1c8ceefbc89
Brian Mak <eggrollbot@gmail.com>
2021-06-16 21:15:03 -0700

    Moved homepage to its own component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import "./App.css";
 import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
-import { Avatar, Box, Button, CssBaseline, Grid, Link, Typography } from "@material-ui/core";
-import pfpImage from "./images/pfp.png";
-import { Code as CodeIcon, GitHub as GitHubIcon, Info as InfoIcon, LinkedIn as LinkedInIcon } from "@material-ui/icons";
+import { CssBaseline } from "@material-ui/core";
+import Home from "./components/Home.js";
 
-function App() {
+function App( ) {
 	const theme = createMuiTheme( {
 		palette: {
 			type: "dark",
@@ -16,12 +15,8 @@ function App() {
 				main: "#FF9B48"
 			}
 		},
-
 		typography: {
-			fontFamily: "\"Raleway\", sans-serif",
-			h2: {
-				fontWeight: "800"
-			}
+			fontFamily: "\"Raleway\", sans-serif"
 		}
 	} );
 
@@ -29,32 +24,7 @@ function App() {
 		<div className="App">
 			<MuiThemeProvider theme={theme}>
 				<CssBaseline />
-
-				<Box mb={3}>
-					<Avatar alt="Eggroll Profile Picture" src={pfpImage} style={{ height: "100px", width: "100px" }}></Avatar>
-				</Box>
-
-				<Box mb={3}>
-					<Typography align="center" fontWeight="fontWeightLight" gutterBottom={true} variant="h2">👋 Hey, I&apos;m Brian.</Typography>
-
-					<Typography align="center" gutterBottom={true} variant="h4">
-						Here is my&nbsp;
-						<Link href="https://github.com/eggroll-bot" rel="noopener noreferrer" style={{color: "#FF845B"}} target="_blank"><GitHubIcon /> GitHub</Link>
-						&nbsp;and&nbsp;
-						<Link color="secondary" href="https://www.linkedin.com/in/brian-mak/" rel="noopener noreferrer" target="_blank"><LinkedInIcon /> LinkedIn</Link>
-						.
-					</Typography>
-				</Box>
-
-				<Grid container align="center" justify="center" spacing={5}>
-					<Grid item>
-						<Button color="primary" size="medium" startIcon={<InfoIcon />} variant="outlined">About Me</Button>
-					</Grid>
-
-					<Grid item>
-						<Button color="primary" size="medium" startIcon={<CodeIcon />} variant="outlined">Portfolio</Button>
-					</Grid>
-				</Grid>
+				<Home />
 			</MuiThemeProvider>
 		</div>
 	);
