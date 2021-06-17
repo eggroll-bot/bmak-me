621a384e99f8dbdf517404459cdc54f52ef35a9e
Brian Mak <eggrollbot@gmail.com>
2021-06-16 17:42:01 -0700

    Finished landing page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from "react";
 import "./App.css";
 import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
-import { Box, Button, CssBaseline, Grid, Typography } from "@material-ui/core";
-import { Code as CodeIcon, /*GitHub as GitHubIcon,*/ Info as InfoIcon, /*LinkedIn as LinkedInIcon*/ } from "@material-ui/icons";
+import { Box, Button, CssBaseline, Grid, Link, Typography } from "@material-ui/core";
+import { Code as CodeIcon, GitHub as GitHubIcon, Info as InfoIcon, LinkedIn as LinkedInIcon } from "@material-ui/icons";
 
 function App() {
 	const theme = createMuiTheme( {
 		palette: {
 			type: "dark",
 			primary: {
-				main: "#27ae60"
+				main: "#FFF640"
+			},
+			secondary: {
+				main: "#FF9B48"
+			}
+		},
+
+		typography: {
+			fontFamily: "\"Raleway\", sans-serif",
+			h2: {
+				fontWeight: "800"
 			}
 		}
 	} );
@@ -20,7 +30,15 @@ function App() {
 				<CssBaseline />
 
 				<Box mb={3}>
-					<Typography align="center" variant="h2" gutterBottom={true}>👋 Hey, I&apos;m Brian.</Typography>
+					<Typography align="center" fontWeight="fontWeightLight" gutterBottom={true} variant="h2">👋 Hey, I&apos;m Brian.</Typography>
+
+					<Typography align="center" gutterBottom={true} variant="h4">
+						Here is my&nbsp;
+						<Link href="https://github.com/eggroll-bot" rel="noopener noreferrer" style={{color: "#FF845B"}} target="_blank"><GitHubIcon /> GitHub</Link>
+						&nbsp;and&nbsp;
+						<Link color="secondary" href="https://www.linkedin.com/in/brian-mak/" rel="noopener noreferrer" target="_blank"><LinkedInIcon /> LinkedIn</Link>
+						.
+					</Typography>
 				</Box>
 
 				<Box mb={3}>
