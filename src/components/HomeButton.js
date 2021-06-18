38cdbb13cce8ed26b10a73537ad52f6d2218c40b
Brian Mak <eggrollbot@gmail.com>
2021-06-17 22:33:14 -0700

    Made HomeButton use props for width and height (default: 100)

--- src/components/HomeButton.js
+++ src/components/HomeButton.js
@@ -1,13 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Avatar } from "@material-ui/core";
 import pfpImage from "../images/pfp.png";
+import PropTypes from "prop-types";
 
-function HomeButton( ) {
+function HomeButton( props ) {
 	return (
-		<div className="HomeButton">
-			<Avatar alt="Home Button" className="Avatar" src={ pfpImage } style={ { height: "100px", width: "100px" } }></Avatar>
+		<div className="HomeButton" style={ { height: props.height || 100, width: props.width || 100 } }>
+			<Link to="/">
+				<Avatar alt="Home Button" className="Avatar" src={ pfpImage } style={ { height: props.height || 100, width: props.width || 100 } }></Avatar>
+			</Link>
 		</div>
 	);
 }
+
+HomeButton.propTypes = {
+	height: PropTypes.number,
+	width: PropTypes.number
+};
 
 export default HomeButton;
