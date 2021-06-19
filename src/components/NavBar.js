4ed1e043f82e8e7a42d34a6a8719feeb79fac0b4
Brian Mak <eggrollbot@gmail.com>
2021-06-19 12:05:07 -0700

    Touched up navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useTheme } from "@material-ui/core/styles";
 import { Avatar, Grid, Typography } from "@material-ui/core";
-import HomeButton from "./HomeButton";
+import { Link } from "react-router-dom";
 import pfpImage from "../images/pfp.png";
 import PropTypes from "prop-types";
 
@@ -14,7 +14,11 @@ function NavBar( props ) {
 		<div className="NavBar" style={ { backgroundColor: theme.palette.background.paper } }>
 			<Grid container>
 				<Grid item xs={ 2 }>
-					<HomeButton padding="15px" />
+					<div style={ { height: 105, padding: "15px", paddingLeft: "30px" } }>
+						<Typography style={ { alignItems: "center", display: "flex", fontSize: "24px", height: 75 } }>
+							<Link to="/">&lt;- Home</Link>
+						</Typography>
+					</div>
 				</Grid>
 
 				<Grid item xs={ 8 }>
@@ -22,7 +26,7 @@ function NavBar( props ) {
 				</Grid>
 
 				<Grid item xs={ 2 }>
-					<div style={ { float: "right", height: 105, width: 105, padding: "15px" } }>
+					<div style={ { float: "right", height: 105, width: 120, padding: "15px", paddingRight: "30px" } }>
 						<Avatar alt="Profile Picture" className="Avatar" src={ pfpImage } style={ { height: 75, width: 75 } }></Avatar>
 					</div>
 				</Grid>
