bfeaeb8871965cfd6f3c6e0da59013fff9c9d52f
Brian Mak <eggrollbot@gmail.com>
2021-06-19 20:52:49 -0700

    Moved "under construction" message

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,7 +5,7 @@ import { Typography } from "@material-ui/core";
 function Portfolio( ) {
 	return (
 		<InnerPage title="🤖 Portfolio">
-			<Typography align="center" style={ { transform: "translateY( calc( 50vh - 250% ) )" } } variant="h3">
+			<Typography align="center" variant="h3">
 					This page is under construction.
 			</Typography>
 		</InnerPage>
