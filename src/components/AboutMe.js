6fc21b25f2cba61981ed44c185ec675a5d521b3b
Brian Mak <eggrollbot@gmail.com>
2021-06-19 20:36:13 -0700

    Finished about me page text

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -18,13 +18,24 @@ function AboutMe( ) {
 
 	const paragraphs = [
 		`
-		Hello! Welcome to my website. I'm Brian Mak. I'm a student at the University of California, Santa Cruz campus majoring in computer science with
-		plans to minor in technology and information management. I like to create open-source side projects in my free time, and have an interest in
-		software development and information technology.
+		Hello! Welcome to my website. I'm Brian Mak. I'm a student at the University of California, Santa Cruz majoring in computer science with plans
+		to minor in technology and information management. I like to create open-source side projects in my free time, and have an interest in software
+		development and system administration.
 		`,
-		"This page is still a work-in-progress.",
+		`
+		In the short term, before I get my bachelor's degree, I'm looking to land a software engineering internship. After I get my degree, I am
+		considering either working in the software engineering industry or pursuing a master's degree in computer science.
+		`,
+		`
+		During the academic year, I am seeking tutoring positions for computer science courses. During my first year at UC Santa Cruz, I tutored for a
+		computer systems and assembly language course. Tutoring for that course turned out to be quite rewarding. As tutors, we are not expected to know
+		everything there is to know about the course's topics, but we are expected to have a good understanding of them. Tutoring for that course solidified
+		my understanding of the course's topics and filled in the gaps. It also helped me further develop my communication and problem solving skills. I
+		hope to take these skills with me down whichever path I decide to take in the future.
+		`,
 		<>
-		Feel free to check out what I&apos;ve been up to recently. My portfolio can be found <Link to="/portfolio">here</Link>.
+			Wanna know what I&apos;ve been up to recently? Feel free to check out my portfolio. My portfolio and résumé can be found&nbsp;
+			<Link to="/portfolio">here</Link>.
 		</>
 	];
 
