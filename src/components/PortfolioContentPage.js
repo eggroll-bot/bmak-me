3ad539108455b9625d50a8d5e027ad0959ed7a3d
Brian Mak <eggrollbot@gmail.com>
2021-07-02 17:47:59 -0700

    Turned PortfolioContentPage into a function component

--- src/components/PortfolioContentPage.js
+++ src/components/PortfolioContentPage.js
@@ -16,53 +16,49 @@ const useStyles = ( ) => ( {
 	}
 } );
 
-class PortfolioContentPage extends React.Component {
-	_renderVideo( item ) {
-		return (
-			<video autoPlay loop muted playsInline src={ item.original } style={ { backgroundColor: "#262626", height: "450px", maxWidth: "100%" } } />
-		);
-	}
+function PortfolioContentPage( props ) {
+	const media = props.media?.map( ( item ) => {
+		const extension = item.match( /[^.]+$/ )[ 0 ];
 
-	render( ) {
-		const media = this.props.media?.map( ( item ) => {
-			const extension = item.match( /[^.]+$/ )[ 0 ];
+		if ( extension === "mp4" ) {
+			return {
+				original: item,
+				renderItem: ( item ) => {
+					const PortfolioVideo = <video autoPlay loop muted playsInline src={ item.original } style={ { backgroundColor: "#262626", height: "450px", maxWidth: "100%" } } />;
 
-			if ( extension === "mp4" ) {
-				return {
-					original: item,
-					renderItem: this._renderVideo.bind( this )
-				};
-			} else {
-				return { original: item };
-			}
-		} );
+					return PortfolioVideo;
+				}
+			};
+		} else {
+			return { original: item };
+		}
+	} );
 
-		return (
-			<InnerPage title={ this.props.title }>
-				{
-					this.props.github &&
+	return (
+		<InnerPage title={ props.title }>
+			{
+				props.github &&
 					<div style={ { paddingBottom: "40px", textAlign: "center" } }>
-						<Button color="primary" href={ this.props.github } rel="noopener noreferrer" size="medium" startIcon={ <GitHubIcon /> } target="_blank" variant="outlined">
+						<Button color="primary" href={ props.github } rel="noopener noreferrer" size="medium" startIcon={ <GitHubIcon /> } target="_blank" variant="outlined">
 							Visit GitHub Repository
 						</Button>
 					</div>
-				}
+			}
 
-				{
-					this.props.media &&
+			{
+				props.media &&
 					<div style={ { paddingBottom: "40px" } }>
 						<ImageGallery items={ media } showFullscreenButton={ false } showPlayButton={ false } showThumbnails={ false } />
 					</div>
-				}
+			}
 
-				{ this.props.paragraphs.map( ( item, index ) => (
-					<Typography align="left" className={ this.props.classes.bodyText } key={ index }>
-						{ item }
-					</Typography>
-				) ) }
-			</InnerPage>
-		);
-	}
+			{ props.paragraphs.map( ( item, index ) => (
+				<Typography align="left" className={ props.classes.bodyText } key={ index }>
+					{ item }
+				</Typography>
+			) ) }
+		</InnerPage>
+	);
 }
 
 PortfolioContentPage.propTypes = {
